ea9fb27ff23f496347433a6a9c95d404324686e7
hammadyounas <m.hammadyounas@outlook.com>
2022-08-31 13:24:50 +0500

    table first time not created issue fixed

--- models/user.model.ts
+++ models/user.model.ts
@@ -8,11 +8,12 @@ class User {
     this.user = user;
   }
 
-  save() {
+  async save() {
     let tableQuery =
       "CREATE TABLE IF NOT EXISTS users(metamask_wallet_account_number VARCHAR(255) PRIMARY KEY, profile_name VARCHAR(100), Bio VARCHAR(512), Timezone VARCHAR(255), primary_currency  ENUM('NAPA','BNB','ETH'), language VARCHAR(255), NAPA_social_media_account_email text)";
 
-    db.execute(tableQuery);
+    await db.execute(tableQuery);
+
     let sql = `INSERT INTO users (metamask_wallet_account_number, profile_name, Bio, Timezone, primary_currency, language, NAPA_social_media_account_email) VALUES ("${this.user.accountNumber}", "${this.user.profileName}", "${this.user.bio}", "${this.user.timezone}", "${this.user.primaryCurrency}", "${this.user.language}", "${this.user.napaSocialMediaAccount}")`;
 
     return db.execute(sql);
