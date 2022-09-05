0a795da747f1fa0ff8617081093cd9ace9ca2288
hammadyounas <m.hammadyounas@outlook.com>
2022-09-05 16:57:23 +0500

    health route added

--- routes/index.routes.ts
+++ routes/index.routes.ts
@@ -1,10 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const UserRouter = require("../routes/user.routes");
 const ChatRouter = require("../routes/chat.routes");
+const HealthRouter = require("../routes/health.routes");
 
 const setUpRoutes = (app) => {
   app.use("/user", UserRouter.router);
   app.use("/chat", ChatRouter.router);
+  app.use("/health", HealthRouter.router);
 };
 
 module.exports = { setUpRoutes };
