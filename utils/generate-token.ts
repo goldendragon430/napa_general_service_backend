6602b4e2b6c4bce65fbc21fab077c97382931d4b
hammadyounas <m.hammadyounas@outlook.com>
2023-01-04 11:00:43 +0500

    changed env variable

--- utils/generate-token.ts
+++ utils/generate-token.ts
@@ -2,13 +2,13 @@
 const jwt = require("jsonwebtoken");
 
 const generateTokenWithPayload = (payload, expiry) => {
-  return jwt.sign(payload, process.env.secret, {
+  return jwt.sign(payload, process.env.SECRET, {
     expiresIn: expiry,
   });
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, process.env.secret);
+  return jwt.verify(token, process.env.SECRET);
 };
 
 module.exports = { generateTokenWithPayload, verifyToken };
