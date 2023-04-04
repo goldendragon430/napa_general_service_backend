2f1ca3338f59420a372ff3b6aba1c2447262bb18
hammadyounas <m.hammadyounas@outlook.com>
2023-04-04 14:03:35 +0500

    insert new record in payouts_tracking table when a new user add

--- interfaces/user.interfaces.ts
+++ interfaces/user.interfaces.ts
@@ -8,4 +8,5 @@ export interface UserInterface {
   napaSocialMediaAccount: string;
   profileId: string;
   avatar: string;
+  tokenPrice: string;
 }
--- models/user.model.ts
+++ models/user.model.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { UserInterface } from "../interfaces/user.interfaces";
 import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 const { db, socialArtDb } = require("../index");
 
 class User {
@@ -24,11 +25,58 @@ class User {
         this.user.timezone || ""
       }", "${this.user.primaryCurrency || "NAPA"}", "${
         this.user.language || "English"
-      }", "${this.user.napaSocialMediaAccount}", "${this.user.avatar || ""}", "false", "false")`;
+      }", "${this.user.napaSocialMediaAccount}", "${
+        this.user.avatar || ""
+      }", "false", "false")`;
 
       await db.execute(insertQuery);
       await socialArtDb.execute(insertQuery);
-      
+
+        const DAUsql = `SELECT profileId FROM users WHERE dailyActive = "true"`;
+        const [DAU] = await socialArtDb.query(DAUsql);
+
+      // @ts-ignore
+      let rewardsTierCap = DAU.length * 0.0005;
+      let category_1, category_2, category_3, category_4, category_5;
+
+      const payoutUuid = uuidv4();
+
+      await Promise.all(
+        Array.from({ length: 5 }).map(async (_, index) => {
+          rewardsTierCap =
+            index == 0 ? rewardsTierCap : rewardsTierCap * 0.2 + rewardsTierCap;
+          if (index == 0) {
+            category_1 = rewardsTierCap;
+            return;
+          } else if (index == 1) {
+            category_2 = rewardsTierCap;
+            return;
+          } else if (index == 2) {
+            category_3 = rewardsTierCap;
+            return;
+          } else if (index == 3) {
+            category_4 = rewardsTierCap;
+            return;
+          } else if (index == 4) {
+            category_5 = rewardsTierCap;
+            return;
+          }
+        })
+      );
+
+      const insertPayoutsTrackingQuery = `INSERT INTO payouts_tracking (updateUUID, category_1, category_2, category_3, category_4, category_5, tokenPrice, createdAt, updatedAt) VALUES (
+        "${payoutUuid}",
+        "${(category_1 / 10).toFixed(8)}",
+        "${(category_2 / 10).toFixed(8)}",
+        "${(category_3 / 10).toFixed(8)}",
+        "${(category_4 / 10).toFixed(8)}",
+        "${(category_5 / 10).toFixed(8)}",
+        "${this.user.tokenPrice || ""}",
+        "${moment(new Date()).format("YYYY-MM-DDTHH:mm:ssZ")}",
+        "${moment(new Date()).format("YYYY-MM-DDTHH:mm:ssZ")}"
+        )`;
+      await socialArtDb.execute(insertPayoutsTrackingQuery);
+
       const sql = `SELECT * FROM users WHERE profileId = "${uuid}" OR accountNumber = "${this.user.accountNumber}"`;
 
       return db.execute(sql);
